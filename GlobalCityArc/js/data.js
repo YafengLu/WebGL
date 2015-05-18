666a7fa52aa7f4cc8f7f436317e330ee58f92c67
unknown <yl186029@WUSYL186029-1OF.TD.TERADATA.COM>
2015-05-18 11:04:02 -0700

    data interface done for GlobalCityArc

---
+++ GlobalCityArc/js/data.js
@@ -0,0 +1,95 @@
+var data = {
+	topCities: [
+	{
+		"name": "Tokyo",
+		"pop": 37883000,
+		"lat": 35.689722,
+		"lng": 139.691667
+	},
+	{
+		
+
+"name": "Los Angeles",
+		"pop": 13131431,
+		"lat": 34.05,
+		"lng": -118.25
+	},
+	{
+		"name": "Seoul",
+		"pop": 
+
+25258000,
+		"lat": 37.566667, 
+		"lng": 126.978056
+	},
+	{
+		"name": "Shanghai",
+		"pop": 24750000,
+		"lat": 
+
+31.2,
+		"lng": 121.5
+	},
+	{
+		"name": "Mexico City",
+		"pop": 21178959,
+		"lat": 19.433333,
+		"lng": -99.133333
+	
+
+},
+	{
+		"name": "Delhi",
+		"pop": 21753486,
+		"lat": 28.61,
+		"lng": 77.23
+	},
+	{
+		"name": "Beijing",
+	
+
+	"pop": 21148000,
+		"lat": 39.913889,
+		"lng": 116.391667
+	},
+	{
+		"name": "Sao Paulo",
+		"pop": 20935204,
+	
+
+	"lat": -23.533,
+		"lng": -46.617
+	},
+	{
+		"name": "Jakarata",
+		"pop": 20000000,
+		"lat": -6.175,
+		"lng": 
+
+106.828611
+	},
+	{
+		"name": "Istanbul",
+		"pop": 14160467,
+		"lat": 41.1575,
+		"lng": 28.868333
+	},
+	{
+		
+
+"name": "New York",
+		"pop": 19949502,
+		"lat": 40.808611,
+		"lng": -74.020386
+	},
+	{
+		"name": "Cape Town",
+		
+
+"pop": 3740026,
+		"lat": -33.925278, 
+		"lng": 18.423889
+	}
+],
+	cityLinks: [{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}}]
+}
--- GlobalCityArc/js/drawCityArcs.js
+++ GlobalCityArc/js/drawCityArcs.js
@@ -1,4 +1,4 @@
-function drawCityArcs(){
+function drawCityArcs(data){
 	  canvas.style("background-color", "#333333");
 
       var controls = new THREE.TrackballControls(camera, canvas.node());
@@ -46,25 +46,9 @@ function drawCityArcs(){
       var earth  = new THREE.MeshPhongMaterial({map: world, shininess: 5});
       var sphere = new THREE.SphereGeometry(200, 40, 40); 
 	  
-	  
-	   d3.json('demos/data/top-cities.json', function (err, cities) {
-	  //d3.json('data/cityLinks.json', function (err, data) {
-        // GENERATE SOME MOCK DATA
-        
-		//var data = [];
-		var arcdata = [{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"Tokyo","pop":37883000,"lat":35.689722,"lng":139.691667},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"Seoul","pop":25258000,"lat":37.566667,"lng":126.978056},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"Mexico City","pop":21178959,"lat":19.433333,"lng":-99.133333},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"Beijing","pop":21148000,"lat":39.913889,"lng":116.391667},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"Jakarata","pop":20000000,"lat":-6.175,"lng":106.828611},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Los Angeles","pop":13131431,"lat":34.05,"lng":-118.25}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Shanghai","pop":24750000,"lat":31.2,"lng":121.5}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Delhi","pop":21753486,"lat":28.61,"lng":77.23}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Sao Paulo","pop":20935204,"lat":-23.533,"lng":-46.617}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Istanbul","pop":14160467,"lat":41.1575,"lng":28.868333}},{"source":{"name":"New York","pop":19949502,"lat":40.808611,"lng":-74.020386},"target":{"name":"Cape Town","pop":3740026,"lat":-33.925278,"lng":18.423889}}];	
-		/*	
-        for (var i = 0; i < cities.length; i+=2) {
-          for (var j = 1; j < cities.length; j+=2) {
-            if (i !== j) {
-              data.push({
-                source: cities[i],
-                target: cities[j]
-              });
-            }
-          }
-        }*/
-		//console.log(data);
+	  var cities = data.topCities;
+	  var arcdata = data.cityLinks;
+	   
         // SELECT INTO THE SCENE
         var root = SubUnit.select(scene);
 
@@ -150,7 +134,6 @@ function drawCityArcs(){
           renderer.render(scene, camera);
         }
         animate();
-		
-      });
+	
 		 
 }
